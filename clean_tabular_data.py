7b94554f60e752076ff65519d4000b279ba3ae7e
ekfiagbedzi <ekfiagbedzi@gmail.com>
2022-08-09 23:14:06 +0000

    Load data without duplicated columns

--- clean_tabular_data.py
+++ clean_tabular_data.py
@@ -2,9 +2,3 @@ import pandas as pd
 
 data = pd.read_csv("Products.csv", lineterminator="\n")
 data
-
-<<<<<<< HEAD
-data.describe(include="object")
-=======
-data.describe(include="object")
->>>>>>> 0225af7f2f2af251344faefb9bc9a2daf24eac00
