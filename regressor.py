9451a79a1199e7047a30a7ad4545c94d25e3d614
ekfiagbedzi <ekfiagbedzi@gmail.com>
2022-08-16 22:17:00 +0000

    Load Data

--- regressor.py
+++ regressor.py
@@ -1,1 +1,7 @@
-from sklearn.feature_extraction.text import CountVectorizer
+#%%
+import pandas as pd
+
+
+data = pd.read_csv("Products.csv", lineterminator="\n", index_col=0, usecols=["product_name", product_description])
+data
+# %%
