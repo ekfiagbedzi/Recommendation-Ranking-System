f3d8ba373d2e89e76256344762177034dfbb39b9
ekfiagbedzi <ekfiagbedzi@gmail.com>
2022-08-21 01:38:12 +0000

    change createtime dtype to datetime

--- utils/clean_tabular_data.py
+++ utils/clean_tabular_data.py
@@ -1,4 +1,12 @@
+# %%
+
 import pandas as pd
 
-data = pd.read_csv("Products.csv", lineterminator="\n")
-data
+data = pd.read_csv("../Products.csv", lineterminator="\n", index_col=0)
+
+data.price = data.price.str.replace("£", "").str.replace(",", "").astype(float)
+data.create_time = pd.to_datetime(data.create_time)
+print(data.dtypes)
+
+data.to_csv("../Products_cleaned.csv")
+# %%
