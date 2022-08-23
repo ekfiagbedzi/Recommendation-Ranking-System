a729a69fff7aeebc415464595c0f5cb720b5fbc6
ekfiagbedzi <ekfiagbedzi@gmail.com>
2022-08-23 22:33:37 +0000

    Combined image dataframe and product dataframe

--- utils/clean_tabular_data.py
+++ utils/clean_tabular_data.py
@@ -6,7 +6,6 @@ data = pd.read_csv("../Products.csv", lineterminator="\n", index_col=0)
 
 data.price = data.price.str.replace("£", "").str.replace(",", "").astype(float)
 data.create_time = pd.to_datetime(data.create_time)
-print(data.dtypes)
 
-data.to_csv("../Products_cleaned.csv")
+data.to_csv("../Products_cleaned.csv", index=0)
 # %%
