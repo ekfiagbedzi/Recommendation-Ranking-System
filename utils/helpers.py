76a5839ff79edf528e2687c0d3b91594e8bd4a14
ekfiagbedzi <ekfiagbedzi@gmail.com>
2022-08-26 17:58:08 +0000

    Add clean image function

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,7 @@
 import cv2
-from numpy import array
 from PIL import Image
+import fnmatch
+
 
 def resize_image(final_size, im):
     """Resize all images to same sizes
@@ -22,6 +23,24 @@ def resize_image(final_size, im):
         (0, 0)) # define coordinate as upper left corner
     return new_im
 
+def clean_image_data(path, final_size, save_path):
+    """Reshape an image to a specified size
+       Args:
+            path: (str) - Location of image
+            final_size: (int) - Size in pixels of image
+            save_path: (str) - Location to store images
+       Returns:
+            New Image with specified size
+    """
+    
+    dirs = fnmatch.filter(os.listdir(path), "*.jpg")
+    for item in dirs:
+        im = Image.open(path + item)
+        new_im = resize_image(final_size, im)
+        item = item.rstrip(".jpg")
+        new_im.save(f"{save_path}/{item}_resized.jpg")
+    print("Images Cleaned Succesfully. Have a Nice Day!!!")
+
 
 def get_element(list, position):
     """Get a member from a pandas series of lists by position
@@ -41,7 +60,8 @@ def image_to_array(img_id):
        Return:
             Array of pixel values
     """
-    img = cv2.imread("/home/ubuntu/Recommendation-Ranking-System/cleaned_images/{}_resized.jpg".format(img_id)
+    img = cv2.imread(
+        "/home/ubuntu/Recommendation-Ranking-System/cleaned_images/{}_resized.jpg".format(img_id)
 )
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
