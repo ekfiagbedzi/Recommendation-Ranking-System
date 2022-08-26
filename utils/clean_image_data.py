8e41f0af1d9ef3071c48dfb6648e31c3cb9d153f
ekfiagbedzi <ekfiagbedzi@gmail.com>
2022-08-26 17:57:16 +0000

    Refactored

--- utils/clean_image_data.py
+++ utils/clean_image_data.py
@@ -1,29 +1,4 @@
-
-from logging import raiseExceptions
-from PIL import Image
-from helpers import resize_image
-import os
-import fnmatch
-
-
-def clean_image_data(path, final_size, save_path):
-    """Reshape an image to a specified size
-       Args:
-            path: (str) - Location of image
-            final_size: (int) - Size in pixels of image
-            save_path: (str) - Location to store images
-       Returns:
-            New Image with specified size
-    """
-    
-    dirs = fnmatch.filter(os.listdir(path), "*.jpg")
-    for item in dirs:
-        im = Image.open(path + item)
-        new_im = resize_image(final_size, im)
-        item = item.rstrip(".jpg")
-        new_im.save(f"{save_path}/{item}_resized.jpg")
-    print("Images Cleaned Succesfully. Have a Nice Day!!!")
-    
+from helpers import clean_image_data
 
 path = "/home/ubuntu/images/"
 save_path = "/home/ubuntu/Recommendation-Ranking-System/cleaned_images/"
