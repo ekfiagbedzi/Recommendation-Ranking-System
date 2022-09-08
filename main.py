6ce9cd45c02b71120f6b2677b3ce6fe43c56fa2d
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-09-08 16:34:48 +0100

    Initial reading of data

---
+++ main.py
@@ -0,0 +1,19 @@
+# %%
+from utils.helpers import get_element, ImageData
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+import torch
+
+# %%
+le = LabelEncoder()
+# %%
+data = pd.read_pickle("image_product.pkl")
+data
+
+
+# %%
+X = data.image_array
+y = le.fit_transform(
+        data.category.str.split("/").apply(get_element, position=0))
+# %%
