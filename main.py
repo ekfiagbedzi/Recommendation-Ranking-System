50bc4fc2d9d074d2ec8f80da5394f4b084e5ae21
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-09-14 15:34:23 +0100

    Dataloadder works

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from sklearn.preprocessing import LabelEncoder
 import torch
+from torch.utils.data import DataLoader
 
 # %%
 le = LabelEncoder()
@@ -18,4 +19,7 @@ y = le.fit_transform(
         data.category.str.split("/").apply(get_element, position=0))
 # %%
 image_data = ImageData(X, y)
+# %%
+loader = DataLoader(image_data, 5, True)
+next(iter(loader))
 # %%
