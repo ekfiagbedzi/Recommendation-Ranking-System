2eaf1d95e3dd416a41f49facf8a15458106f547d
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-09-15 14:00:25 +0100

    Visualised Loss curves on Tensorboard

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 
 
@@ -30,20 +31,20 @@ class NN(torch.nn.Module):
 
 def train(model, epochs=10):
 
+    writer = SummaryWriter()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
-
+    batch_ind = 0
     for epoch in range(epochs):
-        ind = 1
         for batch in loader:
             features, labels = batch
             predictions = model(features)
             loss = F.cross_entropy(predictions, labels)
             loss.backward()
-            print("Batch {}: Loss = {}".format(ind, loss.item()))
+            print("Loss = {}".format(loss.item()))
             optimizer.step()
             optimizer.zero_grad()
-            ind += 1
-            
+            writer.add_scalar("Loss", loss.item(), batch_ind)
+            batch_ind += 1            
         print("Epoch {}: Loss is {}".format(epoch+1, loss.item()))
 
 if __name__ == "__main__":
