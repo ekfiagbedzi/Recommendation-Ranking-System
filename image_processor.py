05ee69c720ad35478d4bea1650dc0a0f57c37c49
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2022-09-20 21:14:49 +0100

    refactored image_processor.py

--- image_processor.py
+++ image_processor.py
@@ -5,17 +5,20 @@ from PIL import Image
 from torchvision import transforms
 
 
-transformers=transforms.Compose([transforms.Resize(size=256),
-            transforms.CenterCrop(size=224),
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomVerticalFlip(p=0.5),
-            transforms.RandomResizedCrop(size=224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])])
+transformers = transforms.Compose(
+    [transforms.Resize(size=256),
+    transforms.CenterCrop(size=224),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.RandomResizedCrop(size=224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                        std=[0.229, 0.224, 0.225])]
+)
 
-img = image_processor("/home/biopythoncodepc/Documents/git_repositories/Recommendation-Ranking-System/data/cleaned_images/0a1baaa8-4556-4e07-a486-599c05cce76c_resized.jpg",
+img = image_processor("data/cleaned_images/0a1baaa8-4556-4e07-a486-599c0"\
+        "5cce76c_resized.jpg",
     transformers=transformers)
 
-print(img.shape)
+print(img, img.shape)
 
