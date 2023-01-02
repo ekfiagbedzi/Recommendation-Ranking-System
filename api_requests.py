9cbff81eff92b3487526f3293c76229d1e0d9b14
Emmanuel Kwasi Fiagbedzi <ekfiagbedzi@gmail.com>
2023-01-02 16:55:21 +0000

    API handler fixed

--- api_requests.py
+++ api_requests.py
@@ -5,7 +5,7 @@ api = FastAPI()
 
 @api.get("/")
 def example_handler():
-    return {"message": "Hello Wo!!!"}
+    return {"message": "Hello World!!!"}
 
 if __name__ == "__main__":
-    uvicorn(api)
+    uvicorn.run(api)
